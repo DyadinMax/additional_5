97cdfe67f8d2e207601aba440f3de713637568cf
DyadinMax <43500937+DyadinMax@users.noreply.github.com>
2018-09-24 23:13:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+    let opening = [];
+    let closing = [];
+    let array = [];
+
+    for (let i = 0; i < str.length; i++) {
+        array.push(str[i]);
+    }
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        opening.push(bracketsConfig[i][0]);
+        closing.push(bracketsConfig[i][1]);
+    }
+
+    for (let i = array.length - 1; i >= 0; i--) {
+            for (let j = 0; j < opening.length; j++) {
+                if (array[i] == opening[j] && array[i + 1] == closing[j]) {
+                    array.splice(i, 2);
+                }
+            }
+    }
+
+    return (array.length == 0);
+};
